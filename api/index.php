1bee1c2ac9266a3042a9fe4555dabb266f09825a
Clint Berry <clint@system-11.com>
2012-09-01 12:59:01 -0600

    updates to php api

--- api/index.php
+++ api/index.php
@@ -4,13 +4,66 @@ require 'Slim/Slim.php';
 
 $app = new Slim();
 
-$app->get('/employees', 'getEmployees');
-$app->get('/employees/:id',	'getEmployee');
-$app->get('/employees/:id/reports',	'getReports');
-$app->get('/employees/search/:query', 'getEmployeesByName');
-$app->get('/employees/modifiedsince/:timestamp', 'findByModifiedDate');
+$app->get('/employees', authorize('user'), 'getEmployees');
+$app->get('/employees/:id',	authorize('user'),'getEmployee');
+$app->get('/employees/:id/reports',	authorize('admin'),'getReports');
+$app->get('/employees/search/:query', authorize('user'),'getEmployeesByName');
+$app->get('/employees/modifiedsince/:timestamp', authorize('user'), 'findByModifiedDate');
+
+// I add the login route which is a post request
+$app->post('/login', 'login');
 
 $app->run();
+// api/index.php
+/**
+ * Quick and dirty login function with hard coded credentials (admin/admin)
+ * This is just an example. Do not use this in a production environment
+ */
+function login() {
+    if(!empty($_POST['username']) && !empty($_POST['password'])) {
+        // normally you would load credentials from a database. 
+        // This is just an example and is certainly not secure
+        if($_POST['username'] == 'admin' && $_POST['password'] == 'admin') {
+            $user = array("username"=>"admin", "firstName"=>"Clint", "lastName"=>"Berry", "role"=>"user");
+            $_SESSION['user'] = $user;
+            echo json_encode($user);
+        }
+        else {
+            echo '{"error":{"text":"You shall not pass..."}}';
+        }
+    }
+    else {
+        echo '{"error":{"text":"Username and Password are required."}}';
+    }
+}
+
+/**
+ * Authorise function, used as Slim Route Middlewear
+ */
+function authorize($role = "user") {
+    return function () use ( $role ) {
+        // Get the Slim framework object
+        $app = Slim::getInstance();
+        // First, check to see if the user is logged in at all
+        if(!empty($_SESSION['user'])) {
+            // Next, validate the role to make sure they can access the route
+            // We will assume admin role can access everything
+            if($_SESSION['user']['role'] == $role || 
+                $_SESSION['user']['role'] == 'admin') {
+                //User is logged in and has the correct permissions... Nice!
+                return true;
+            }
+            else {
+                // If a user is logged in, but doesn't have permissions, return 403
+                $app->halt(403, 'You shall not pass!');
+            }
+        }
+        else {
+            // If a user is not logged in at all, return a 401
+            $app->halt(401, 'You shall not pass!');
+        }
+    }
+}
 
 function getEmployees() {
 
